9c5678b2672d6eb8147c73f476deb3ca3d513a98
Sabah97 <sabah.ashraf97@gmail.com>
2019-10-30 08:41:16 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 /* Add your script here */
+
+//Task 1
+$("#list").click(function(){
+    $.ajax({
+        url:'http://skeleton.mernbook.com/api/users',
+        method: 'GET'
+       
+    }).done(function(response){
+        response.forEach(element=>{
+            let user=$('<p>').text(element.name)
+            $('#users').append(user)
+        })
+        //console.log(response);
+    }).fail(function(response){
+        console.log(response);
+    })
+})
